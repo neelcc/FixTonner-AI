873dfce32011429f2b67ef00cb4bc149fbaba098
neelcc <neel.c.chaurasiya@gmail.com>
2025-08-10 10:03:57 +0530

    db setup

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import ConnectDB from './db/index.js'
+
+dotenv.config({
+    path: './env'
+})
+
+ConnectDB()
+
 const app = express()
 const PORT = process.env.PORT || 4000
+console.log(process.env.PORT);
+
 
 app.get('/' , (req,res) => {
     res.send("Server is ready")
